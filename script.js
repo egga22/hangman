257c7e04e7b64b512aaac1ea1c9fe5a0af222342
egga22 <84204028+egga22@users.noreply.github.com>
2025-05-30 13:28:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,88 +1,66 @@
-var g = 0
-	var words = [
-		"penguin",
-		"hangman",
-		"universe",
-		"treehouse",
-		"vacation",
-		"inside",
-		"yellow",
-		"school",
-		"everybody",
-		"clock",
-		"book",
-		"animal",
-		"human",
-		"overall",
-		"rainbow",
-		"fireplace",
-		"pumpkin",
-		"dogtoy",
-		"waterfall",
-		"slippers",
-		"weasel",
-		"beanbag",
-		"rocketship",
-		"family",
-		"hamster",
-		"light",
-		"scared",
-		"intergalactic",
-		"tomorrow",
-		"recces",
-		"vacine",
-		"covid19",
-		"magical"
-	];
-	
-	var word = words[Math.floor(Math.random() * words.length)];
-	var answerArray = [];
-	for (var i = 0; i < word.length; i++) {
-		answerArray[i] = "_";
-	}
-	
-	var remainingLetters = word.length;
-	
-	while (remainingLetters > 0){
-		
-		var guess = prompt("Guess a letter, or click Cancel to stop playing. By the way so far you have " + answerArray.join(" "));
-		if (guess === null) {
-			var c =confirm ("Confirm?")
-			if (c == true) {
-				alert("Bye!");
-				break;
-			}
-		} else if (guess === "hint") {
-		// hint //
-			g = g + 1
-			var hv = true
-			while (hv == true) {
-				var tr = word[Math.floor(Math.random() * word.length)];
-				var hv = answerArray.includes(tr);
-			}
-			alert (tr);
-			
-		
-		
-		} else if (guess === word) {
-			alert("You got it early!");
-			break;
-		} else if (guess.length < 1) {
-			alert("You didn't enter in anything!");
-		} else if (guess.length !== 1) {
-			alert("Incorrect!");
-			alert("game over!");
-			break;
-		} else {
-			g = g + 1
-			for (var j = 0; j < word.length; j++) {
-				if (word[j] === guess) {
-					answerArray[j] = guess;
-					remainingLetters--;
-				}
-			}
-		}
-	}	
-	alert(answerArray.join(" "));
-	alert("It took you " + g + " guesses!")
-	alert("The game has ended! (The answer was " + word + ")");
+var g = 0;
+var words = [
+  "penguin", "hangman", "universe", "treehouse", "vacation", "inside", "yellow",
+  "school", "everybody", "clock", "book", "animal", "human", "overall",
+  "rainbow", "fireplace", "pumpkin", "dogtoy", "waterfall", "slippers", "weasel",
+  "beanbag", "rocketship", "family", "hamster", "light", "scared", "intergalactic",
+  "tomorrow", "recces", "vacine", "covid19", "magical"
+];
+
+var word = words[Math.floor(Math.random() * words.length)];
+var answerArray = Array(word.length).fill("_");
+var remainingLetters = word.length;
+
+console.log("Welcome to Hangman! Type `play()` to start playing.");
+
+function play() {
+  while (remainingLetters > 0) {
+    let guess = prompt("Guess a letter (or full word), or type 'hint'.\n" + answerArray.join(" "));
+
+    if (guess === null) {
+      if (confirm("Are you sure you want to quit?")) {
+        alert("Thanks for playing!");
+        return;
+      } else {
+        continue;
+      }
+    }
+
+    guess = guess.toLowerCase();
+
+    if (guess === "hint") {
+      g++;
+      let revealed = false;
+      while (!revealed) {
+        let tr = word[Math.floor(Math.random() * word.length)];
+        if (!answerArray.includes(tr)) {
+          alert("Hint letter: " + tr);
+          revealed = true;
+        }
+      }
+    } else if (guess === word) {
+      alert("Correct! You guessed the word.");
+      break;
+    } else if (guess.length !== 1) {
+      alert("Please enter a single letter.");
+      continue;
+    } else {
+      g++;
+      let correctGuess = false;
+      for (let j = 0; j < word.length; j++) {
+        if (word[j] === guess && answerArray[j] === "_") {
+          answerArray[j] = guess;
+          remainingLetters--;
+          correctGuess = true;
+        }
+      }
+      if (!correctGuess) {
+        alert("Incorrect guess.");
+      }
+    }
+  }
+
+  alert("Word: " + answerArray.join(" "));
+  alert("Guesses: " + g);
+  alert("Game over! The word was: " + word);
+}
